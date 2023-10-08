a13cc8146b7b21d10d2a07dd497797df03119345
Khushi080 <99126360+Khushi080@users.noreply.github.com>
2023-10-08 21:47:38 +0530

    Update lawyer_recommender.py

--- lawyer_recommender.py
+++ lawyer_recommender.py
@@ -2,29 +2,29 @@ import streamlit as st
 import pickle
 lawyer_data = pickle.load(open("lawyers_list.pkl", 'rb'))
 similarity = pickle.load(open("similarity.pkl", 'rb'))
-lawyer_names = lawyer_data['Rating'].tolist() 
+lawyer_names = lawyer_data['Name'].tolist()
 st.header("Lawyer recommender")
-selected_lawyer = st.selectbox("Select lawyers you want", lawyer_names)
+selected_lawyer = st.selectbox("Select a lawyer", lawyer_names)
 
-def recommend(lawyer_data, target_name):
-    index = int(lawyer_data[lawyer_data['Name'] == target_name].index[0])
+def recommend(target_name):
+    index = lawyer_data[lawyer_data['Name'] == target_name].index[0]
     distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector: vector[1])
-    store=[]
-    for i in distance[0:5]:
+    store = []
+    for i in distance[1:6]:
         store.append(lawyer_data.iloc[i[0]]['Name'])
     return store
 
 if st.button("Show recommendation"):
-    name=recommend(selected_lawyer)
-    col1,col2,col3,col4,col5=st.columns(5)
+    recommended_lawyers = recommend(selected_lawyer)
+    col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
-        st.text(name[0])
+        st.text(recommended_lawyers[0])
     with col2:
-        st.text(name[1])
+        st.text(recommended_lawyers[1])
     with col3:
-        st.text(name[2])
+        st.text(recommended_lawyers[2])
     with col4:
-        st.text(name[3])
+        st.text(recommended_lawyers[3])
     with col5:
-        st.text(name[4])
+        st.text(recommended_lawyers[4])
     st.write("Selected lawyer:", selected_lawyer)
